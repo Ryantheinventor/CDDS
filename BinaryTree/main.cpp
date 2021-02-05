5cdc360ac428ce4768f63d49bf3d8d5b69ecf1d8
Ryantheinventor <ryantheinventor1@gmail.com>
2021-02-05 10:03:56 -0800

    Updated mains for better readability in output

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -17,6 +17,7 @@ int main()
 	bTree.insert(6);
 	bTree.insert(7);
 	bTree.insert(10);
+	std::cout << "bTree.insert(3); bTree.insert(2); bTree.insert(5); bTree.insert(4); bTree.insert(8); bTree.insert(9); bTree.insert(1); bTree.insert(6); bTree.insert(7); bTree.insert(10); \n";
 	tBinaryTree<int>::vertex *found;
 	for (int i = 0; i < 11; i++) 
 	{
@@ -34,18 +35,28 @@ int main()
 	tBinaryTree<int> bTree3;
 	bTree3 = bTree;*/
 
+	std::cout << "InOrder\n";
 	bTree.printInOrder();
+	std::cout << "PostOrder\n";
 	bTree.printPostOrder();
+	std::cout << "PreOrder\n";
 	bTree.printPreOrder();
 
 	tBinaryTree<int>::vertex *found2;
 	bTree.search(8, found2);
 	bTree.remove(found2);
+	std::cout << "InOrder after search and remove 8\n";
 	bTree.printInOrder();
 
 	tBinaryTree<int> clearTree = tBinaryTree<int>();
+	clearTree.insert(3);
+	clearTree.insert(2);
 	clearTree.clear();
+	clearTree.insert(6);
+	clearTree.insert(4);
 
+	std::cout << "InOrder after insert 3 and 2, clear, insert 6 and 4\n";
+	clearTree.printInOrder();
 
 	return 0;
 }
--- CDDS/main.cpp
+++ CDDS/main.cpp
@@ -11,15 +11,21 @@ int main()
 	{
 		std::cout << "empty\n";
 	}
+	std::cout << "push front (5) 3x\n";
 	dList.push_front(5);
 	dList.push_front(5);
 	dList.push_front(5);
+	if (!dList.empty())
+	{
+		std::cout << "not empty\n";
+	}
+	std::cout << "clear\n";
 	dList.clear();
 	if (dList.empty())
 	{
 		std::cout << "empty\n";
 	}
-
+	std::cout << "push front 1-10 in order\n";
 	dList.push_front(1);
 	dList.push_front(2);
 	dList.push_front(3);
@@ -30,18 +36,29 @@ int main()
 	dList.push_front(8);
 	dList.push_front(9);
 	dList.push_front(10);
+	std::cout << "iterator loop front to back\n";
 	for (auto it = dList.begin(); it != dList.end(); ++it)
 	{
 		std::cout << (*it) << std::endl;
 	}
 	std::cout << "\n";
 
+	std::cout << "iterator loop reverse\n";
 	for (auto it = dList.rbegin(); it != dList.rend(); --it)
 	{
 		std::cout << (*it) << std::endl;
 	}
 	std::cout << "\n";
 	dList.resize(2);
+	std::cout << "iterator loop front to back after resize\n";
+	for (auto it = dList.begin(); it != dList.end(); ++it)
+	{
+		std::cout << (*it) << std::endl;
+	}
+	std::cout << "\n";
+
+	std::cout << "iterator loop reverse after resize\n";
+	
 	for (auto it = dList.rbegin(); it != dList.rend(); --it)
 	{
 		std::cout << (*it) << std::endl;
@@ -57,20 +74,33 @@ int main()
 	dList.push_front(10);
 	dList.push_front(10);
 	dList.push_front(10);
-	dList.push_front(10);
-	dList.push_front(10);
-	dList.push_front(10);
-	dList.remove(10);
-
-	for (auto it = dList.rbegin(); it != dList.rend(); --it)
+	dList.push_back(10);
+	dList.push_back(10);
+	dList.push_back(10);
+	std::cout << "pushfront/back many 10s\n";
+	std::cout << "in order iterator before remove 10\n";
+	for (auto it = dList.begin(); it != dList.end(); ++it)
 	{
 		std::cout << (*it) << std::endl;
 	}
+	std::cout << "\n";
+	dList.remove(10);
 
-	if (dList.empty())
+	std::cout << "in order iterator after remove 10\n";
+	for (auto it = dList.begin(); it != dList.end(); ++it)
 	{
-		std::cout << "empty\n";
+		std::cout << (*it) << std::endl;
+	}
+	std::cout << "\n";
+
+	if (!dList.empty())
+	{
+		std::cout << "not empty before clear\n";
 	}
 	dList.clear();
+	if (dList.empty())
+	{
+		std::cout << "empty after clear\n";
+	}
 	return 0;
 }
